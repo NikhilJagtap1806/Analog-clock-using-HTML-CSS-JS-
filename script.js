9527ba252a09e62fd8316eefbdcb6fdb6da47ccd
NikhilJagtap1806 <jagtapnikhil1806@gmail.com>
2024-01-26 23:53:44 +0530

    basic functionality added

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let clk=document.getElementsByClassName("clock")
 let hr=document.getElementById("hour")
 let min=document.getElementById("min")
 let sec=document.getElementById("sec")
+
+function displaytime(){
+    let date=new Date();
+
+    let hrh=date.getHours();
+    let minh=date.getMinutes();
+    let sech=date.getSeconds();
+
+}
+setInterval(displaytime,1000)
