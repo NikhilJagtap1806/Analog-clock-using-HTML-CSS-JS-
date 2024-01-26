a2a90ce67ab7cd5cefd62fadd5e715a112f9edbd
NikhilJagtap1806 <jagtapnikhil1806@gmail.com>
2024-01-27 00:30:40 +0530

    Analog clock completed

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function displaytime(){
     let minh=date.getMinutes();
     let sech=date.getSeconds();
 
+    let hr_rotation=hrh*30+minh/2;
+    let min_rotation=minh*6;
+    let sec_rotation=sech*6;
+
+
+    hr.style.transform=`rotate(${hr_rotation}deg)`;
+    min.style.transform=`rotate(${min_rotation}deg)`;
+    sec.style.transform=`rotate(${sec_rotation}deg)`;
+
 }
 setInterval(displaytime,1000)
